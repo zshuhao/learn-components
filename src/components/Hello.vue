c3fc32cf32be94987329559d3b84cda4fbfd0a78
zhangshuhao <zhang.shuhao@mwee.cn>
2019-09-30 15:45:24 +0800

    provide

---
+++ src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+      hello
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    mounted () {
+    }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <button @click="handleTest">测试按钮</button>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+        HelloWorld
+    },
+    data () {
+        return {
+            obj: {
+                name: '000'
+            }
+        }
+    },
+    methods: {
+        handleTest () {
+            console.log(this.obj)
+            this.obj.name = 'lele'
+            console.log(this.obj)
+        }
+    }
 }
 </script>
